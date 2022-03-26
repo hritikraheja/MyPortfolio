f0d7c5a9d47c00217eaa51cbf3015c65a7ca2bfd
hritikRaheja <hritikraheja27@gmail.com>
2022-03-26 11:05:06 +0530

    Created copy phone number to clipboard button.

--- copyToClipboard.js
+++ copyToClipboard.js
@@ -1,4 +1,8 @@
 function copyPhoneNumberToClipBoard(){
     navigator.clipboard.writeText("6398945909");
-    
+    var a = document.getElementById('copied_message');
+    a.style.display = "block";
+    setTimeout(function(){
+        a.style.display = "none";
+    }, 1300);
 }
